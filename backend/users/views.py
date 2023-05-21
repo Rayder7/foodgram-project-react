f51b1ce9dd3ffceb841a1b542c31f1b762431a38
Александр Мочалин <mochalin.av7@gmail.com>
2023-05-21 18:44:12 +0500

    fix flake8

--- backend/users/views.py
+++ backend/users/views.py
@@ -31,6 +31,10 @@ class UserViewSet(UserViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         if request.method == 'DELETE':
+            if get_object_or_404(
+                Follow, user=user,
+                    author=author).DoesNotExist:
+                return Response(status=status.HTTP_204_NO_CONTENT)
             get_object_or_404(Follow, user=user, author=author).delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
 
