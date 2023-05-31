ca668cfd3191d17b98bc9e2e8ac93c205752016a
Александр Мочалин <mochalin.av7@gmail.com>
2023-05-31 21:25:44 +0500

    v2

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,9 +1,8 @@
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
-from users.serializers import UserSerializer
-
 from recipes.models import (Favorite, Ingredient, IngredientToRecipe, Recipe,
                             ShopList, Tag)
+from rest_framework import serializers
+from users.serializers import UserSerializer
 
 
 class TagSerializer(serializers.ModelSerializer):
