1c0ad4fd1e471f3cefef29ca977337f46983d1af
Александр Мочалин <mochalin.av7@gmail.com>
2023-06-01 14:17:01 +0500

    fix login and redoc

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,7 +119,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthenticatio',
+        'rest_framework.authentication.TokenAuthentication',
     ],
 }
 
